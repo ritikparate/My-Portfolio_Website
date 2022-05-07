a13214d9b4f4223e8ace9d7dcbaf8c9427b318fc
ritikparate <ritikparate1@gmail.com>
2022-05-07 15:43:28 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('#portfolio a').click(function(ev) {
+      ev.preventDefault();
+      $('#portfolio .project-thumbnail').addClass('fadeOut');
+      $('#portfolio .project-thumbnail' + this.dataset.filter).removeClass('fadeOut');
+    });
+  });
